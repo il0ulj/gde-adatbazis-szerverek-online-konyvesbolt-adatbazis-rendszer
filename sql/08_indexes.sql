b9c4f9cab7ae5c5e0745f8c708ffe5808565c947
il0ulj <il0ulj@neptun.gde.hu>
2024-10-06 06:55:16 +0000

    In 08_indexes.sql titles modified

--- sql/08_indexes.sql
+++ sql/08_indexes.sql
@@ -4,7 +4,10 @@
 
 \c konyvesbolt
 
---- vasarlo tábla
+--- rendelesek tábla
 
 CREATE INDEX idx_rendelesek_vasarlo_datum ON rendelesek (vasarlo_id, rend_datum DESC);
+
+--- vasarlo tábla
+
 CREATE INDEX idx_vasarlo_id ON vasarlo (vasarlo_id);
